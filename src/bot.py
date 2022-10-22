43b40ba2182c7942a1c2ac38c3f475f9e0d84c42
mraniki <8766259+mraniki@users.noreply.github.com>
2022-10-22 18:05:55 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,19 +1,2 @@
-import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-if __name__ == '__main__':
-    application = ApplicationBuilder().token('TOKEN').build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    
-    application.run_polling()
+message = 'Please wait while the program is loading...'
+print(message)
