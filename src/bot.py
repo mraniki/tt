4743f36c6e1729140c8b1035a5d057bc0bc96f11
mraniki <8766259+mraniki@users.noreply.github.com>
2022-10-22 20:07:47 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -20,8 +20,7 @@ logger = logging.getLogger(__name__)
 # VAR
 telegram_tkn = getenv("TOKEN")
 ALLOWED_USER_ID = getenv("ALLOWED_USER_ID")
-parser = argparse.ArgumentParser(
-    description="Script to download files from Telegram Channel.")
+parser = argparse.ArgumentParser(description="INT Transformation")
 parser.add_argument("--user-id", required=True, type=int, Default=ALLOWED_USER_ID)
 args = parser.parse_args()
 user_id = args.user_id
