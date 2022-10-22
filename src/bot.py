1511881b71e2d37dca29200e27812a45aedf998f
mraniki <8766259+mraniki@users.noreply.github.com>
2022-10-22 18:57:03 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -12,23 +12,23 @@ message = 'Please wait while the program is loading...'
 print(message)
 
 
-if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+# Enable logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-  #  c_dir = os.path.dirname(__file__)
-  #  with open(os.path.join(c_dir, "/config/secrets.txt")) as key_file:
-  #      api_key, secret, telegram_tkn, user_id = key_file.read().splitlines()
+telegram_tkn = getenv("TOKEN")
+user_id = getenv("ALLOWED_USER_ID")
+exchange_id1 = getenv("EXCHANGE1")
+exchange_id1_api = getenv("EXCHANGE1YOUR_API_KEY")  
+exchange_id1_secret = getenv("EXCHANGE1YOUR_SECRET") 
 
-   telegram_tkn = getenv("TOKEN")
-   user_id = getenv("ALLOWED_USER_ID")
-   exchange_env = getenv("EXCHANGE")  
         
-    ccxt_ex = ccxt.exchange_env()
-    ccxt_ex.apiKey = api_key
-    ccxt_ex.secret = secret
+ccxt_ex = ccxt.exchange_id1
+ccxt_ex.apiKey = exchange_id1_api
+ccxt_ex.secret = exchange_id1_secret
 
-    exchange = CryptoExchange(ccxt_ex)
-    trade_executor = TradeExecutor(exchange)
-    telegram_bot = TelegramBot(telegram_tkn, user_id, trade_executor)
+exchange = CryptoExchange(ccxt_ex)
+trade_executor = TradeExecutor(exchange)
+telegram_bot = TelegramBot(telegram_tkn, user_id, trade_executor)
 
-    telegram_bot.start_bot()
+telegram_bot.start_bot()
