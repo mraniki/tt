3efcb7ce759e789e2aa314f8c1472e5d254f1806
mraniki <8766259+mraniki@users.noreply.github.com>
2022-10-22 18:45:17 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,2 +1,34 @@
+import logging
+import os
+import ccxt
+
+from core.exchange import CryptoExchange
+from core.telegrambot import TelegramBot
+from core.tradeexcutor import TradeExecutor
+
+from os import getenv
+
 message = 'Please wait while the program is loading...'
 print(message)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+  #  c_dir = os.path.dirname(__file__)
+  #  with open(os.path.join(c_dir, "/config/secrets.txt")) as key_file:
+  #      api_key, secret, telegram_tkn, user_id = key_file.read().splitlines()
+
+   telegram_tkn = getenv("TOKEN")
+   user_id = getenv("ALLOWED_USER_ID")
+        
+        
+    ccxt_ex = ccxt.binance()
+    ccxt_ex.apiKey = api_key
+    ccxt_ex.secret = secret
+
+    exchange = CryptoExchange(ccxt_ex)
+    trade_executor = TradeExecutor(exchange)
+    telegram_bot = TelegramBot(telegram_tkn, user_id, trade_executor)
+
+    telegram_bot.start_bot()
