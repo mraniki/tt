6dedbd1934aea61367bf687bba3a1dd536b69506
mraniki <8766259+mraniki@users.noreply.github.com>
2022-10-22 22:23:35 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,21 +1,35 @@
 import logging
+
+from telegram import __version__ as TG_VER
+
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+
+if __version_info__ < (20, 0, 0, "alpha", 1):
+    raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+        f"{TG_VER} version of this example, "
+        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+    )
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
 import os
 import ccxt
 import argparse
-
-from core.exchange import CryptoExchange
-from core.telegrambot import TelegramBot
-from core.tradeexecutor import TradeExecutor
-
 from os import getenv
 
 message = 'Please wait while the program is loading...'
 print(message)
 
-
-# Enable logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 # VAR
 telegram_tkn = getenv("TOKEN")
@@ -27,8 +41,6 @@ user_id = args.user_id
 
 if not user_id:
     logger.warning('user_id not set, you will not be able to control the bot')
-
-
 
 exchange_id = getenv("EXCHANGE1")
 exchange_id1_api = getenv("EXCHANGE1YOUR_API_KEY")  
@@ -42,15 +54,45 @@ ccxt_ex = exchange_class({
 })
 
 
-exchange = CryptoExchange(ccxt_ex)
-#print(exchange.fetch_balance())
-#print(exchange.free_balance)
-
-
-trade_executor = TradeExecutor(exchange)
-
 
 #BOT
-telegram_bot = TelegramBot(telegram_tkn, user_id, trade_executor)
-telegram_bot.start_bot()
 
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("TOKEN").build()
+
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
+
+
+if __name__ == "__main__":
+ main()
