e1054c40846de8cc7cc70cf51171442cd08afd73
mraniki <8766259+mraniki@users.noreply.github.com>
2022-10-23 09:59:40 +0200

    update

--- src/bot.py
+++ src/bot.py
@@ -6,17 +6,24 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-import ccxt
+
+
+import os
 import argparse
+from dotenv import load_dotenv
 from os import getenv
-import core.telegrambot 
+from pathlib import Path
+
+dotenv_path = Path('.env')
+load_dotenv(dotenv_path=dotenv_path)
+
 
 message = 'Please wait while the program is loading...'
 print(message)
 
 
 # VAR
-telegram_tkn = getenv("TOKEN")
+telegram_tkn = os.getenv("TOKEN")
 ALLOWED_USER_ID = getenv("ALLOWED_USER_ID")
 parser = argparse.ArgumentParser(description="INT Transformation")
 parser.add_argument("--user-id", required=False, type=int, default=ALLOWED_USER_ID)
@@ -26,21 +33,40 @@ user_id = args.user_id
 if not user_id:
     logger.warning('user_id not set, you will not be able to control the bot')
 
-exchange_id = getenv("EXCHANGE1")
+exchange_id1 = getenv("EXCHANGE1")
 exchange_id1_api = getenv("EXCHANGE1YOUR_API_KEY")  
 exchange_id1_secret = getenv("EXCHANGE1YOUR_SECRET") 
 
-#EXCHANGE
-exchange_class = getattr(ccxt, exchange_id)
-ccxt_ex = exchange_class({
-    'apiKey': exchange_id1_api,
-    'secret': exchange_id1_secret,
-})
 
 
 
 #BOT
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(selective=True),
+    )
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
+
+async def bal_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text(balance)
 
 def main() -> None:
     """Start the bot."""
@@ -49,6 +75,7 @@ def main() -> None:
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("bal", bal_command))
     application.add_handler(CommandHandler("help", help_command))
 
     # on non command i.e message - echo the message on Telegram
@@ -58,5 +85,22 @@ def main() -> None:
     application.run_polling()
 
 
+
+
+
+#EXCHANGE
+import ccxt
+
+exchange_id = exchange_id1
+exchange_class = getattr(ccxt, exchange_id)
+exchange = exchange_class({
+    'apiKey': exchange_id1_api,
+    'secret': exchange_id1_secret,
+})
+balance = exchange.fetch_free_balance()
+
+
+
 if __name__ == "__main__":
  main()
+
