be7c49ad1750976644108f383ae3a160a2c2fadc
mraniki <8766259+mraniki@users.noreply.github.com>
2022-10-27 20:02:36 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -22,7 +22,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
-TTVersion="0.6.2"
+TTVersion="0.6.3"
 
 print('TT', TTVersion)
 print('python', sys.version)
