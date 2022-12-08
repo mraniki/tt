43e70f742d43ad59460f6f24b656f109ed463027
mraniki <8766259+mraniki@users.noreply.github.com>
2022-12-08 16:08:36 +0100

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -101,8 +101,8 @@ menu=f'{TTVersion} \n {commandlist}\n'
 
 ##▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 ##====== common functions  =============
-def sendmessage (string)
-    await update.effective_chat.send_message(f"{string}", parse_mode=constants.ParseMode.HTML)
+# def sendmessage (string)
+#     await update.effective_chat.send_message(f"{string}", parse_mode=constants.ParseMode.HTML)
 
 def Convert(string):
    li = list(string.split(" "))
