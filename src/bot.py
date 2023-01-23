80bbf05c3334bd780032c395ef622da5aa524d8b
mraniki <8766259+mraniki@users.noreply.github.com>
2023-01-23 13:23:33 +0000

    Jan 23, 2023, 2:23 PM

--- src/bot.py
+++ src/bot.py
@@ -254,7 +254,8 @@ async def LoadExchange(exchangeid, mode):
 def tokenlist_search(parsedJson, name):
     for entry in parsedJson:
         if name == entry ['symbol']:
-            return entry ['address']
+            if chainId==entry ['chainId']:
+                return entry ['address']
     # logger.info(msg=f"name {name} chainId {chainId}")
     # for dict in json_object:
     #     if (dict['chainId'] == chainId):
