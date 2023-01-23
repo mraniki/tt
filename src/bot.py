eff4bb554836fb00159ed641700d0ece4d017cf5
mraniki <8766259+mraniki@users.noreply.github.com>
2023-01-23 20:27:43 +0000

    Jan 23, 2023, 9:27 PM

--- src/bot.py
+++ src/bot.py
@@ -242,9 +242,12 @@ async def LoadExchange(exchangeid, mode):
         ex = Web3(Web3.HTTPProvider('https://'+networkprovider))
         #ex = Web3(Web3.HTTPProvider(networkprovider))
         router_instanceabi= await DEXFetchAbi(router) #Router ABI
+        quoter_instanceabi= await DEXFetchAbi('0x61fFE014bA17989E743c5F6cB21bF9697530B21e') #Quoter ABI
         logger.info(msg=f"Router ABI {router_instanceabi}")
+        logger.info(msg=f"Quoter ABI {quoter_instanceabi}")
         Web3.to_checksum_address
         router_instance = ex.eth.contract(address=ex.to_checksum_address(router), abi=router_instanceabi) #ContractLiquidityRouter
+        quoter_instance = ex.eth.contract(address=ex.to_checksum_address('0x61fFE014bA17989E743c5F6cB21bF9697530B21e'), abi=quoter_instanceabi) #ContractLiquidityRouter
         if ex.net.listening:
             logger.info(msg=f"Connected to {ex}")
             return name
