c3cdd345c01ce71c078844b6cca67287b25f7ce4
mraniki <8766259+mraniki@users.noreply.github.com>
2023-01-24 16:23:58 +0000

    Jan 24, 2023, 5:23 PM

--- src/bot.py
+++ src/bot.py
@@ -498,7 +498,7 @@ async def SendOrder_DEX(s1,s2,s3,s4,s5):
         elif (version=="v3"):
             fee=int(3000)
             sqrt_price_limit_x96 = 0
-            _amountOutRaw= quoter_instance.functions.quoteExactInputSingle(tokenToBuy, tokenToSell, fee, OrderAmount, sqrt_price_limit_x96).call()
+            _amountOutRaw= quoter_instance.functions.quoteExactInputSingle(tokenToBuy, tokenToSell, fee, int(amountTosell), sqrt_price_limit_x96).call()
         #     swap_TX=router_instance.functions.swapExactTokensForTokens(tokenToBuy,tokenToSell,3000,walletaddress,deadline,OrderAmount,0,0)
         #     tx_token = await DEX_Sign_TX(swap_TX)
         # elif (version =="limitorder"):
