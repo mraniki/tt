d8feb37ad3b87e9bf0938b4a5a19fad418431d17
mraniki <8766259+mraniki@users.noreply.github.com>
2023-01-24 15:49:51 +0000

    Jan 24, 2023, 4:49 PM

--- src/bot.py
+++ src/bot.py
@@ -457,14 +457,10 @@ async def SendOrder_DEX(s1,s2,s3,s4,s5):
         else:
             tokenA=s2
             tokenB=basesymbol
-        logger.info(msg=f"ORDER {tokenA} {tokenB}")
-        logger.info(msg=f"SendOrder_DEX")
-        logger.info(msg=f"{tokenA}")
         tokenToSell=ex.to_checksum_address(await DEXContractLookup(tokenA))
-        logger.info(msg=f"tokenToSell {tokenToSell}")
-        AbiTokenA= await DEXFetchAbi(tokenToSell) #tokenToSell ABI
-        #logger.info(msg=f"AbiTokenA {AbiTokenA}")
+        AbiTokenA= await DEXFetchAbi(tokenToSell) 
         contractTokenA = ex.eth.contract(address=tokenToSell, abi=AbiTokenA) 
+        tokenToBuy= ex.to_checksum_address(await DEXContractLookup(tokenB))
         # approvalcheck = contractTokenA.functions.allowance(ex.to_checksum_address(walletaddress), ex.to_checksum_address(router)).call()
         # logger.info(msg=f"approvalcheck {approvalcheck}")
         # if (approvalcheck==0):
@@ -473,9 +469,6 @@ async def SendOrder_DEX(s1,s2,s3,s4,s5):
         #     ApprovaltxHash = await DEX_Sign_TX(approval_TX)
         #     logger.info(msg=f"Approval {str(ex.to_hex(ApprovaltxHash))}")
         #     time.sleep(10) #wait approval
-        logger.info(msg=f"tokenToBuy {await DEXContractLookup(tokenB)}")
-        tokenToBuy= ex.to_checksum_address(await DEXContractLookup(tokenB))
-        logger.info(msg=f"tokenToBuy {s2} {tokenToBuy}")
         OrderPath=[tokenToSell, tokenToBuy]
         tokeninfobal=contractTokenA.functions.balanceOf(walletaddress).call()
         tokeninfobaldecimal=contractTokenA.functions.decimals().call()
