f07e8fe806099b95bd6b0544cd26152901488cd3
mraniki <mraniki@users.noreply.github.com>
2023-04-24 11:41:56 +0200

    fix: Update bot and config files

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@
 from dynaconf import Dynaconf
 
 settings = Dynaconf(
-    envvar_prefix="TT",
+    #envvar_prefix="TT",
     settings_files=['settings.toml', '.secrets.toml'],#,'example.toml'],
     load_dotenv=True,
     environments=True,
