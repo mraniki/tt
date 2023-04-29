f821c858716ecd6cc5c3942104fb155deb880456
mraniki <mraniki@users.noreply.github.com>
2023-04-29 23:45:51 +0200

    fix: Remove unnecessary logging configuration in bot.py

--- src/bot.py
+++ src/bot.py
@@ -34,15 +34,6 @@ from ping3 import ping
 # #🧐LOGGING
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=settings.loglevel)
 logger = logging.getLogger(__name__)
-if settings.loglevel=='DEBUG':
-    logging.getLogger('ccxt').setLevel(logging.WARNING)
-    logging.getLogger('urllib3').setLevel(logging.WARNING)
-    logging.getLogger('telegram').setLevel(logging.WARNING)
-    logging.getLogger('apprise').setLevel(logging.WARNING)
-    logging.getLogger('telethon').setLevel(logging.WARNING)
-    logging.getLogger('discord').setLevel(logging.WARNING)
-    logging.getLogger('simplematrixbotlib').setLevel(logging.WARNING)
-
 
 #🔁UTILS
 async def parse_message(self,msg):
