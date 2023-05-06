983d25f4d374124c4c29af09b8338ebe2b243c7d
mraniki <mraniki@users.noreply.github.com>
2023-05-06 07:41:46 +0200

    :zap:removing config validator due to default settings

--- src/config.py
+++ src/config.py
@@ -14,17 +14,17 @@ settings = Dynaconf(
     load_dotenv=True,
     environments=True,
     default_env="default",
-    validators=[
-        Validator("loglevel", default="INFO", apply_default_on_none=True),
-        Validator("host", default="0.0.0.0", apply_default_on_none=True),
-        Validator("port", default=8080, apply_default_on_none=True),
-        Validator("bot_prefix", must_exist=True, default=["/", "!"],
-                  apply_default_on_none=True),
-        Validator("bot_token", must_exist=True,
-                  messages={"You forgot to set {bot_token}in settings."}),
-        Validator("bot_channel_id", must_exist=True,
-                  messages={"You forgot to set {bot_channel_id} in settings."})
-          ]
+    #validators=[
+        #Validator("loglevel", default="INFO", apply_default_on_none=True),
+        #Validator("host", default="0.0.0.0", apply_default_on_none=True),
+       # Validator("port", default=8080, apply_default_on_none=True),
+        #Validator("bot_prefix", must_exist=True, default=["/", "!"],
+                  #apply_default_on_none=True),
+        #Validator("bot_token", must_exist=True,
+                  #messages={"You forgot to set {bot_token}in settings."}),
+        #Validator("bot_channel_id", must_exist=True,
+                  #messages={"You forgot to set {bot_channel_id} in settings."})
+        #  ]
 )
 
 #  🧐LOGGING
