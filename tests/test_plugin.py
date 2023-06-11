c879a9dc30b2dfbc23b232d5cc4d285b78de3fd6
mraniki <8766259+mraniki@users.noreply.github.com>
2023-06-11 18:20:43 +0200

    ✅ unit test

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,5 +1,11 @@
 from unittest.mock import MagicMock
 import pytest
+import sys
+import os
+
+# Add the project root directory to the sys.path list
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 from talky.config import settings, logger, PluginManager
 from unittest.mock import MagicMock
