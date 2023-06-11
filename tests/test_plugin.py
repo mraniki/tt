e0ecfb3fad60788427c641d859780b3ac838103d
mraniki <8766259+mraniki@users.noreply.github.com>
2023-06-11 18:31:39 +0200

    ✅ unit test

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,52 +1,56 @@
-from unittest.mock import MagicMock
-import pytest
-import sys
-import os
-
-# Add the project root directory to the sys.path list
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-
-from talky.config import settings, logger, PluginManager
+from types import ModuleType
 from unittest.mock import MagicMock
 import pytest
 
-from talky.config import settings, logger, PluginManager
+from talky.config import PluginManager
+
+class MockPlugin:
+    is_plugin = True
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+class MockPackage(ModuleType):
+    mock_plugin = MockPlugin()
+
+    @staticmethod
+    def __getattr__(name):
+        if name == "mock_plugin":
+            return MockPlugin()
+        raise AttributeError(f"module {__name__} has no attribute {name}")
 
 class TestPluginManager:
     @pytest.fixture
     def plugin_manager(self):
         return PluginManager()
 
-    @pytest.fixture
-    def mock_plugin(self):
-        plugin = MagicMock()
-        plugin.is_plugin = True
-        return plugin
-
-    def test_load_plugins(self, plugin_manager, mock_plugin):
+    def test_load_plugins(self, plugin_manager):
         # Mock the importlib and pkgutil modules
         importlib_mock = MagicMock()
         pkgutil_mock = MagicMock()
         importlib_mock.import_module.return_value = pkgutil_mock
-        pkgutil_mock.iter_modules.return_value = [(None, "mock_plugin", None)]
+        pkgutil_mock.iter_modules.return_value = [(None, "mock_package", None)]
         mock_module = MagicMock()
-        mock_module.__name__ = "mock_plugin"
-        mock_module.mock_plugin = mock_plugin
-        pkgutil_mock.import_module.return_value = mock_module
+        mock_module.__name__ = "mock_package"
+        mock_module.mock_plugin = MockPlugin()
+        pkgutil_mock.import_module.return_value = MockPackage()
         plugin_manager.plugins = {}
         plugin_manager._importlib = importlib_mock
         plugin_manager._pkgutil = pkgutil_mock
 
-        # Load plugins from the mock package
-        plugin_manager.load_plugins("mock_package")
+        # Load plugins from the mock_package package
+        plugin_manager.load_plugins("talky.plugins")
 
         # Check that the plugin was loaded correctly
         assert len(plugin_manager.plugins) == 1
         assert "mock_plugin" in plugin_manager.plugins
 
-    def test_start_plugin(self, plugin_manager, mock_plugin):
+    def test_start_plugin(self, plugin_manager):
         # Mock the plugin and start method
+        mock_plugin = MagicMock()
         mock_plugin.start.return_value = None
         plugin_manager.plugins = {"mock_plugin": mock_plugin}
 
@@ -56,8 +60,9 @@ class TestPluginManager:
         # Check that the plugin was started correctly
         mock_plugin.start.assert_called_once()
 
-    def test_stop_plugin(self, plugin_manager, mock_plugin):
+    def test_stop_plugin(self, plugin_manager):
         # Mock the plugin and stop method
+        mock_plugin = MagicMock()
         mock_plugin.stop.return_value = None
         plugin_manager.plugins = {"mock_plugin": mock_plugin}
 
