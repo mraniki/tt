6e89e243b8ea935274d54c7cad898287bd5f407f
mraniki <8766259+mraniki@users.noreply.github.com>
2023-06-12 07:16:56 +0200

    Update test_plugin.py

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,10 +1,17 @@
 import pytest
-# from unittest.mock import patch, MagicMock
 from tt.config import PluginManager
 
 @pytest.mark.asyncio
 async def test_load_plugins():
     plugin_manager = PluginManager()
     plugin_manager.load_plugins("tt.plugins")
-    await plugin_manager.start_plugin("TalkyTrendPlugin")
+
+    print("Loaded plugins:", plugin_manager.plugins)
+
+    for plugin_name, plugin_instance in plugin_manager.plugins.items():
+        print("Plugin:", plugin_name)
+        print("Plugin instance:", plugin_instance)
+
+        await plugin_instance.start()
+
     assert len(plugin_manager.plugins) == 1
