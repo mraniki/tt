5041063810574759584a21f5af41ccfb211d28b0
mraniki <8766259+mraniki@users.noreply.github.com>
2023-06-12 07:38:17 +0200

    Update talkytrend_plugin.py

--- tt/plugins/talkytrend_plugin.py
+++ tt/plugins/talkytrend_plugin.py
@@ -7,17 +7,28 @@ class TalkyTrendPlugin(BasePlugin):
     def __init__(self):
         self.logger = logging.getLogger(name="TalkyTrend")
         self.talky_trend = None
+        self.enabled = settings.talkytrend_enabled
 
     async def start(self):
+        if not self.enabled:
+            return
+
         self.logger.info("Starting TalkyTrend service...")
         self.talky_trend = TalkyTrend()
         asyncio.create_task(self.scanner())
 
     async def stop(self):
+        if not self.enabled:
+            return
+
         self.logger.info("Stopping TalkyTrend service...")
 
     async def scanner(self):
         while True:
+            if not self.enabled:
+                await asyncio.sleep(settings.scanner_frequency)
+                continue
+
             messages = await self.talky_trend.check_signal()
             if messages:
                 for message in messages:
