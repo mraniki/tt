49d4e52d6315fd0f02978178e0747fc880352d99
mraniki <8766259+mraniki@users.noreply.github.com>
2023-06-19 19:28:07 +0200

    ♻️ plugin cleanup

--- tt/plugins/cex_exchange_plugin.py
+++ tt/plugins/cex_exchange_plugin.py
@@ -8,47 +8,34 @@ class CexExchangePlugin(BasePlugin):
     """CEX Plugin"""
     name = os.path.splitext(os.path.basename(__file__))[0]
     def __init__(self):
-        try:
-            self.enabled = settings.cex_enabled
-            if self.enabled:
-                self.fmo = FindMyOrder()
-                if settings.cex_name:
-                    client = getattr(ccxt, settings.cex_name)
-                    self.exchange = client({
-                        'apiKey': settings.cex_api,
-                        'secret': settings.cex_secret,
-                        'password': (settings.cex_password or ''),
-                        'enableRateLimit': True,
-                        'options': {
-                            'defaultType': settings.cex_defaulttype,
-                                    }})
-                    if settings.cex_testmode:
-                        self.exchange.set_sandbox_mode('enabled')
-                    print(self.exchange)
-        except Exception as error:
-            logger.warning(error)
+        self.enabled = settings.cex_enabled
+        if self.enabled:
+            self.fmo = FindMyOrder()
+            if settings.cex_name:
+                client = getattr(ccxt, settings.cex_name)
+                self.exchange = client({
+                    'apiKey': settings.cex_api,
+                    'secret': settings.cex_secret,
+                    'password': (settings.cex_password or ''),
+                    'enableRateLimit': True,
+                    'options': {
+                        'defaultType': settings.cex_defaulttype,
+                                }})
+                if settings.cex_testmode:
+                    self.exchange.set_sandbox_mode('enabled')
+
 
     async def start(self):
         """Starts the exchange_plugin plugin"""
-        try:
-            pass
-        except Exception as error:
-            logger.warning(error)
 
     async def stop(self):
         """Stops the exchange_plugin plugin"""
-        try:
-            pass
-        except Exception as error:
-            logger.warning(error)
+
 
     async def send_notification(self, message):
         """Sends a notification"""
-        try:
-            if self.enabled:
-                await send_notification(message)
-        except Exception as error:
-            logger.warning(error)
+        if self.enabled:
+            await send_notification(message)
 
     def should_handle(self, message):
         """Returns True if the plugin should handle the message"""
@@ -56,23 +43,32 @@ class CexExchangePlugin(BasePlugin):
 
     async def handle_message(self, msg):
         """Handles incoming messages"""
-        try:
-            if self.enabled and await self.fmo.search(msg):
+        if self.enabled:
+            if msg.startswith(settings.bot_ignore):
+                return
+            if await self.fmo.search(msg):
                 order = await self.fmo.get_order(msg)
                 trade = await self.execute_order(order)
                 if trade:
                     await send_notification(trade)
-            command = (msg.split(" ")[0])[1:]
-            if command == settings.bot_command_quote:
-                symbol = msg.split(" ")[1]
-                await self.send_notification(
+            if msg.startswith(settings.bot_prefix):
+                command = (msg.split(" ")[0])[1:]
+                if command == settings.bot_command_quote:
+                    symbol = msg.split(" ")[1]
+                    await self.send_notification(
                     f"🏦 {await self.exchange.fetchTicker(symbol)}")
-            elif command == settings.bot_command_bal:
-                await self.send_notification(f"{await self.get_account_balance()}")
-            elif command == settings.bot_command_pos:
-                await self.send_notification(f"{await self.get_account_position()}")
-        except Exception as error:
-            logger.warning(error)
+                elif command == settings.bot_command_bal:
+                    await self.send_notification(f"{await self.get_account_balance()}")
+                elif command == settings.bot_command_pos:
+                    await self.send_notification(f"{await self.get_account_position()}")
+                elif command == settings.bot_command_help:
+                    await self.send_notification(self.info_message())
+
+    def info_message(self):
+        """info_message"""    
+        exchange_name = self.exchange.id
+        account_info = self.exchange.uid
+        return f"💱 {exchange_name}\n🪪 {account_info}"
 
     async def execute_order(self, order_params):
         """Execute order."""
@@ -117,54 +113,35 @@ class CexExchangePlugin(BasePlugin):
         except Exception as e:
             return f"⚠️ order execution: {e}"
 
+    async def get_trading_asset_balance(self):
+        """return main asset balance."""
+        return self.exchange.fetchBalance()[f"{settings.trading_asset}"]["free"]
+
+
     async def get_account_balance(self):
         """return account balance."""
         balance = "🏦 Balance\n"
-        try:
-            raw_balance = self.exchange.fetch_free_balance()
-            filtered_balance = {k: v for k, v in
-                                raw_balance.items()
-                                if v is not None and v > 0}
-            balance += "".join(f"{iterator}: {value} \n" for
-                            iterator, value in
-                            filtered_balance.items())
-            if not balance:
-                balance += "No Balance"
-            return balance
-        except Exception as error:
-            logger.warning(error)
+        raw_balance = self.exchange.fetch_free_balance()
+        filtered_balance = {k: v for k, v in
+                            raw_balance.items()
+                            if v is not None and v > 0}
+        balance += "".join(f"{iterator}: {value} \n" for
+                        iterator, value in
+                        filtered_balance.items())
+        if not balance:
+            balance += "No Balance"
+        return balance
+
 
     async def get_account_position(self):
         """return account position."""
-        try:
-            open_positions = self.exchange.fetch_positions()
-            open_positions = [p for p in open_positions if p['type'] == 'open']
-            position = "📊 Position\n" + str(open_positions)
-            position += str(await self.exchange.fetch_balance({'type': 'margin',}))
-            return position
-        except Exception as e:
-            return f"⚠️ account_position: {e}"
+        open_positions = self.exchange.fetch_positions()
+        open_positions = [p for p in open_positions if p['type'] == 'open']
+        position = "📊 Position\n" + str(open_positions)
+        position += str(await self.exchange.fetch_balance({'type': 'margin',}))
+        return position
 
-    async def get_name(self):
-        """Return exchange name"""
-        try:
-            return self.exchange.id
-        except Exception as error:
-            logger.warning(error)
 
-    async def get_trading_asset_balance(self):
-        """return main asset balance."""
-        try:
-            return self.exchange.fetchBalance()[f"{settings.trading_asset}"]["free"]
-        except Exception as error:
-            logger.warning(error)
-
-    async def get_account(self):
-        """Return exchange account"""
-        try:
-            return self.exchange.uid
-        except Exception as error:
-            logger.warning(error)
 
 
 
--- tt/plugins/helper_plugin.py
+++ tt/plugins/helper_plugin.py
@@ -9,38 +9,24 @@ from tt.config import logger, settings
 class HelperPlugin(BasePlugin):
     name = os.path.splitext(os.path.basename(__file__))[0]
     def __init__(self):
-        try:
-            self.enabled = settings.helper_enabled
-            if self.enabled:
-                self.version = f"🗿 {__version__}\n"
-                #self.latency = ping3.ping(settings.ping, unit='ms')
-                #round(response_time, 3)
-                self.host_ip = self.get_host_ip()
-                self.help_message = settings.bot_msg_help
-        except Exception as error:
-            logger.warning(error)
+        self.enabled = settings.helper_enabled
+        if self.enabled:
+            self.version = f"🗿 {__version__}\n"
+            self.host_ip = self.get_host_ip()
+            self.help_message = settings.bot_msg_help
 
     async def start(self):
         """Starts the plugin"""
-        try:
-            pass
-        except Exception as error:
-            logger.warning(error)
+        await self.send_notification(self.help_command())
 
     async def stop(self):
         """Stops the plugin"""
-        try:
-            pass
-        except Exception as error:
-            logger.warning(error)
+        pass
 
     async def send_notification(self, message):
         """Sends a notification"""
-        try:
-            if self.enabled:
-                await send_notification(message)
-        except Exception as error:
-            logger.warning(error)
+        if self.enabled:
+            await send_notification(message)
 
     def should_handle(self, message):
         """Returns plugin status"""
@@ -48,40 +34,38 @@ class HelperPlugin(BasePlugin):
 
     async def handle_message(self, msg):
         """Handles incoming messages"""
-        try:
-            if self.enabled:
-                if msg == f"{settings.bot_prefix}{settings.bot_command_help}":
-                    msg = f"{self.version}\n🕸️ {self.host_ip}\n🏓 {ping3.ping(settings.ping, unit='ms')}\n{self.help_message}"
-                    await self.send_notification(msg)
-                elif msg == f"{settings.bot_prefix}{settings.bot_command_trading}":
+        if self.enabled:
+            if msg.startswith(settings.bot_ignore):
+                return
+            if msg.startswith(settings.bot_prefix):
+                command = (msg.split(" ")[0])[1:]
+                if command == settings.bot_command_help:
+                    await self.send_notification(self.help_command())
+                elif command == settings.bot_command_trading:
                     await self.send_notification(self.trading_switch_command())
-        except Exception as error:
-            logger.warning(error)
-
-    def get_host_ip(self):
-        """Returns host IP """
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.connect((settings.ping, 80))
-            ip_address = s.getsockname()[0]
-            s.close()
-            return ip_address
-        except Exception:
-            pass
-
-        # if msg.startswith(settings.bot_ignore):
-        #     return
-        # # Check bot command
-        # if msg.startswith(settings.bot_prefix):
-        #     # message = None
-        #     command = (msg.split(" ")[0])[1:] 
-        
+                elif command == settings.bot_command_restart:
+                    await self.send_notification(self.restart_command())
     
-        
-    async def trading_switch_command(self):
+    def help_command(self):
+        """Help Message"""
+        return (f"🗿 {self.version}\n"
+                f"🕸️ {self.host_ip}\n"
+                f"🏓 {round(ping3.ping(settings.ping, unit='ms'), 3)}\n"
+                f"{self.help_message}")
+
+    def trading_switch_command(self):
+        """Trading switch command"""
         settings.trading_enabled = not settings.trading_enabled
         return f"Trading is {'enabled' if settings.trading_enabled else 'disabled'}."
     
-    async def restart_command(self):
-        # Restart bot
+    def restart_command(self):
+        """Restart"""
         os.execl(sys.executable, os.path.abspath(__file__), sys.argv[0])
+
+    def get_host_ip(self):
+        """Returns host IP """
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect((settings.ping, 80))
+        ip_address = s.getsockname()[0]
+        s.close()
+        return ip_address
