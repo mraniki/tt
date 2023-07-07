203bea0cd9eb9b1ca67a16e3f3b7b302a53c9c53
mraniki <8766259+mraniki@users.noreply.github.com>
2023-07-07 16:39:48 +0200

    📝 Docs

--- docs/conf.py
+++ docs/conf.py
@@ -37,11 +37,11 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
+    'sphinx_copybutton',
     'myst_parser',
     'sphinx_inline_tabs',
-    'sphinx_copybutton',
-    'sphinx_reredirects',
-    'sphinx_design',
+    # 'sphinx_reredirects',
+    # 'sphinx_design',
     'notfound.extension'
 
 ]
