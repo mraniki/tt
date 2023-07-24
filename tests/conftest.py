056eab021c176ff5fbbcc8b5b4f0347b538ca979
mraniki <8766259+mraniki@users.noreply.github.com>
2023-07-24 12:35:37 +0200

    🥚 🔊 loguru implementation

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from _pytest.logging import LogCaptureFixture
+from loguru import logger
+
+
+@pytest.fixture
+def caplog(caplog: LogCaptureFixture):
+    handler_id = logger.add(
+        caplog.handler,
+        format="{message}",
+        level=0,
+        filter=lambda record: record["level"].no >= caplog.handler.level,
+        enqueue=False,  # Set to 'True' if your test is spawning child processes.
+    )
+    yield caplog
+    logger.remove(handler_id)
