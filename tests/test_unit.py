273957cf39abf672843251dc4057d9c55f558ca3
mraniki <mraniki@users.noreply.github.com>
2023-07-25 21:40:03 +0200

    :white_check_mark:

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -96,7 +96,7 @@ async def test_start_plugins():
 
 
 @pytest.mark.asyncio
-async def test_run_bot(caplog):
+async def test_run_bot():
     start_bot = AsyncMock()
     with patch('tt.utils.start_bot', start_bot):
         task = asyncio.create_task(run_bot())
