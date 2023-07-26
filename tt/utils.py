e3bb3e2c7f3c3d687f6a1d03ea082f4ddadfd29c
mraniki <8766259+mraniki@users.noreply.github.com>
2023-07-26 21:59:15 +0200

    🚑 for start_bot iteration

--- tt/utils.py
+++ tt/utils.py
@@ -50,11 +50,12 @@ async def start_bot(listener, plugin_manager, max_iterations=None):
     await listener.start()
     await start_plugins(plugin_manager)
     iteration = 0
-    while iteration < max_iterations:
+    while True:
         msg = await listener.handler.get_latest_message()
         if msg and settings.plugin_enabled:
             await plugin_manager.process_message(msg)
         iteration += 1
-
+        if max_iterations is not None and iteration >= max_iterations:
+            break
+        
     await asyncio.sleep(1)
-    
