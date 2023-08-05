9bd750f95697e2363f9deef9fed80364d67903b2
mraniki <8766259+mraniki@users.noreply.github.com>
2023-08-05 18:05:34 +0200

    :ambulance: Update bot.py

--- tt/bot.py
+++ tt/bot.py
@@ -59,5 +59,5 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host=settings.host,
-        port=int(settings.port),
-        log_level="CRITICAL")
+        port=int(settings.port),)
+
