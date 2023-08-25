95bb1e6540adf2c45b1cd7e94137bfc289711543
mraniki <8766259+mraniki@users.noreply.github.com>
2023-08-25 19:07:25 +0200

    ⚗️ testing vue

--- tt/bot.py
+++ tt/bot.py
@@ -21,7 +21,8 @@ import requests
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+
+# from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from tt.config import settings
