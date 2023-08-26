ed89725fe90819b4cd47a2bae177d7a0b8af11de
mraniki <mraniki@users.noreply.github.com>
2023-08-26 23:46:33 +0200

    Refactor file paths in bot.py and fix stylesheet link in index.html

--- tt/bot.py
+++ tt/bot.py
@@ -29,8 +29,7 @@ from tt.utils import __version__, run_bot, send_notification
 
 app = FastAPI(title="TALKYTRADER")
 
-#os.path.join(os.path.dirname(__file__), 
-templates = Jinja2Templates("./ui/templates")
+templates = Jinja2Templates(os.path.join(os.path.dirname(__file__), "ui/templates"))
 app.mount("/", StaticFiles(directory=os.path.dirname(__file__)), name="/")
 
 @app.on_event("startup")
