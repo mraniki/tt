74d84077d13611d44f549abd28b4d0a432fbd083
mraniki <mraniki@users.noreply.github.com>
2023-08-26 21:36:51 +0200

    Refactor static file path in bot.py

--- tt/bot.py
+++ tt/bot.py
@@ -30,7 +30,7 @@ from tt.utils import __version__, run_bot, send_notification
 app = FastAPI(title="TALKYTRADER")
 
 templates = Jinja2Templates("./ui/templates")
-app.mount("./ui/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="./ui/static"), name="static")
 
 
 @app.on_event("startup")
