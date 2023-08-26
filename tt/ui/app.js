1c9c27ec4bedd484bdebcb23e9daef8361b8667c
mraniki <8766259+mraniki@users.noreply.github.com>
2023-08-26 20:24:55 +0200

    ⚗️

--- tt/ui/app.js
+++ tt/ui/app.js
@@ -3,7 +3,7 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 createApp({
   data() {
     return {
-      message: 'Hello World (via VueJS-CDN)'
+      message: 'Hello Vue!'
     }
   }
 }).mount('#app')
