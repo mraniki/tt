85cea7a426338413c4d621fcbbc996d0d13c4d8b
mraniki <mraniki@users.noreply.github.com>
2023-08-27 13:57:01 +0200

    Update bot.py and index.html files

--- tt/bot.py
+++ tt/bot.py
@@ -34,11 +34,11 @@ templates = Jinja2Templates(
 )
 
 
-app.mount(
-    "/static",
-    StaticFiles(directory=os.path.join(os.path.dirname(__file__), "static")),
-    name="static",
-)
+# app.mount(
+#     "/static",
+#     StaticFiles(directory=os.path.join(os.path.dirname(__file__), "static")),
+#     name="static",
+# )
 
 
 @app.on_event("startup")
