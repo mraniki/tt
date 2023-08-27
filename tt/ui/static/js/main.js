5f331422eca84297352b710618a73b37a13a767b
mraniki <8766259+mraniki@users.noreply.github.com>
2023-08-27 21:37:42 +0200

    🐛 fix uvicorn https://stackoverflow.com/questions/73450371/url-for-is-using-http-instead-of-https-in-fastapi

--- tt/ui/static/js/main.js
+++ tt/ui/static/js/main.js
@@ -1,10 +1,10 @@
-alert('Hello, World!');
-
+import { createApp }  from 'https://unpkg.com/vue@3/dist/vue.global.js'
 // import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
-// import App from './App.vue'
+import App from './App.vue'
 
 
-// const app = createApp(App)
-// app.mount('#app')
+const app = createApp(App)
+app.config.compilerOptions.delimiters = ['[[', ']]']
+app.mount("#app")
 
-{/* <script src="{{ url_for('static', filename='js/main.js') }}" ></script> */}
+<script src="{{ url_for('static', filename='js/main.js') }}" ></script>
