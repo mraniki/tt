0d995f7de65226fa2b5144b1dec3bf1f01d1683c
mraniki <8766259+mraniki@users.noreply.github.com>
2023-08-27 19:33:07 +0200

    ⚗️ helloworld.vue

--- tt/ui/static/js/app.js
+++ tt/ui/static/js/app.js
@@ -1,12 +1,7 @@
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
-
-createApp({
-  data() {
-    return {
-      message: 'Hello Vue!'
-    }
-  }
-}).mount('#app')
+import App from './App.vue'
 
 
+const app = createApp(App)
+app.mount('#app')
 
