d31fe04f6f5c06b8e621a0d122d49b8a9d257a3c
mraniki <8766259+mraniki@users.noreply.github.com>
2023-08-28 11:30:18 +0200

    ♻️

--- tt/ui/static/js/main.js
+++ tt/ui/static/js/main.js
@@ -1,9 +1,11 @@
-// import { createApp }  from 'https://unpkg.com/vue@3/dist/vue.global.prod.js'
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js'
-import App from './App.vue'
+// // import { createApp }  from 'https://unpkg.com/vue@3/dist/vue.global.prod.js'
+// import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js'
+// import App from './App.vue'
 
 
-const app = createApp(App)
-// app.config.compilerOptions.delimiters = ['[[', ']]']
-app.mount("#app")
+// const app = createApp(App)
+// // app.config.compilerOptions.delimiters = ['[[', ']]']
+// app.mount("#app")
 
+// <div id="app"></div>
+// <script type="module" src="{{ url_for('static', filename='js/main.js') }}" ></script> 
