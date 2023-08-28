728abb3665cb3f4cf5eea235dd60b67618c26414
mraniki <mraniki@users.noreply.github.com>
2023-08-28 18:46:13 +0200

    :rotating_light:

--- tt/frontend/main.py
+++ tt/frontend/main.py
@@ -7,12 +7,12 @@ def init(fastapi_app: FastAPI) -> None:
     @ui.page('/show')
     def show():
         ui.label('Hello, FastAPI!')
-
-        # NOTE dark mode will be persistent for each user across tabs and server restarts
-        ui.dark_mode().bind_value(app.storage.user, 'dark_mode')
-        ui.checkbox('dark mode').bind_value(app.storage.user, 'dark_mode')
+        ui.dark_mode().bind_value(
+            app.storage.user, 'dark_mode')
+        ui.checkbox('dark mode').bind_value(
+            app.storage.user, 'dark_mode')
 
     ui.run_with(
         fastapi_app,
-        storage_secret='pick your private secret here',  # NOTE setting a secret is optional but allows for persistent storage per user
+        storage_secret='pick',  
         )
