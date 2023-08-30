9d387aaefedeea3e84f54605cc70a9b52a8d4e4f
mraniki <8766259+mraniki@users.noreply.github.com>
2023-08-30 08:08:41 +0200

    🎨

--- tt/frontend/main.py
+++ tt/frontend/main.py
@@ -45,7 +45,6 @@ def init(fastapi_app: FastAPI) -> None:
                     <!-- TradingView Widget BEGIN -->
             <div class="tradingview-widget-container">
             <div class="tradingview-widget-container__widget"></div>
-            <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
             <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
             {
             "symbols": [
