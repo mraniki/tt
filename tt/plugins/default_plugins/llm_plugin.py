38c5d4e7055a606ab172ecaa6444d4e01f7c5581
mraniki <8766259+mraniki@users.noreply.github.com>
2023-09-26 18:49:49 +0200

    💄 Update AI continous support to filter items

--- tt/plugins/default_plugins/llm_plugin.py
+++ tt/plugins/default_plugins/llm_plugin.py
@@ -37,7 +37,9 @@ class LlmPlugin(BasePlugin):
         """Handles incoming messages"""
         if not self.should_handle(msg):
             return
-        if self.llm.llm_ai_mode:
+        if self.llm.llm_ai_mode and not (
+            msg.startswith(settings.llm_prefix) or msg.startswith(settings.bot_ignore)
+        ):
             await self.send_notification(f"{await self.llm.chat(str(msg))}")
         if msg.startswith(settings.bot_prefix):
             command, *args = msg.split(" ")
