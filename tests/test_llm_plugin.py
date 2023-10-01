7d6befe15d9c973c9e912a977e284cf518fa26e2
mraniki <mraniki@users.noreply.github.com>
2023-10-01 19:30:56 +0200

    Refactor test_llm_plugin.py

--- tests/test_llm_plugin.py
+++ tests/test_llm_plugin.py
@@ -12,7 +12,6 @@ def set_test_settings():
     settings.configure(FORCE_ENV_FOR_DYNACONF="testing")
 
 
-
 @pytest.fixture(name="plugin")
 def test_fixture_plugin():
     return LlmPlugin()
@@ -34,7 +33,7 @@ async def test_plugin(plugin):
 async def test_plugin_notification(plugin):
     """Test notification"""
     plugin.send_notification = AsyncMock()
-    await plugin.handle_message(f"{settings.bot_prefix}{settings.bot_command_question}")
+    await plugin.handle_message(f"{settings.bot_prefix}{settings.bot_command_aichat}")
     plugin.send_notification.assert_awaited_once()
 
 
@@ -50,7 +49,7 @@ async def test_parsing_llm(plugin):
     """Test scr"""
     plugin.llm.chat = AsyncMock()
     await plugin.handle_message(
-        f"{settings.bot_prefix}{settings.bot_command_question} hello"
+        f"{settings.bot_prefix}{settings.bot_command_aichat} hello"
     )
     plugin.llm.chat.assert_awaited_once()
 
