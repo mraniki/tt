a2dd1b6de112352c37a0b294e667203703c1e418
mraniki <8766259+mraniki@users.noreply.github.com>
2024-04-08 08:50:51 +0200

    ⚡ update cex_exchange_plugin.py and dex_exchange_plugin.py

--- tt/plugins/default_plugins/cex_exchange_plugin.py
+++ tt/plugins/default_plugins/cex_exchange_plugin.py
@@ -52,14 +52,13 @@ class CexExchangePlugin(BasePlugin):
         """
         if not self.should_handle(msg):
             return
-        if (settings.bot_ignore not in msg or settings.bot_prefix not in msg) and (
-            await self.fmo.search(msg) and self.should_handle_timeframe()
-        ):
-            order = await self.fmo.get_order(msg)
-            if order and settings.trading_enabled:
-                trade = await self.exchange.submit_order(order)
-                if trade:
-                    await send_notification(trade)
+        if settings.bot_ignore not in msg or settings.bot_prefix not in msg:
+            if await self.fmo.search(msg) and self.should_handle_timeframe():
+                order = await self.fmo.get_order(msg)
+                if order and settings.trading_enabled:
+                    trade = await self.exchange.submit_order(order)
+                    if trade:
+                        await send_notification(trade)
 
         if msg.startswith(settings.bot_prefix):
             command, *args = msg.split(" ")
