9acf371e73dae923a367712a08f98451fe05e0dd
mraniki <mraniki@users.noreply.github.com>
2024-06-17 09:17:41 +0200

    Update import statement in __init__.py for Notifier.

--- tt/utils/ __init__.py
+++ tt/utils/ __init__.py
@@ -6,7 +6,7 @@
 __version__ = "4.9.3"
 
 #from .bot import Bot
-from .notification import Notifier
+from .notifications import Notifier
 from .utils import run_bot
 
 __all__ = ["Notifier", "run_bot"]#, "Bot"]
