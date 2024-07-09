1d65dc21cf44f93a4c31551a9f48aa4406303f59
mraniki <8766259+mraniki@users.noreply.github.com>
2024-07-09 16:07:58 +0200

    ♻️ feed plugin

--- tt/plugins/default_plugins/feed_plugin.py
+++ tt/plugins/default_plugins/feed_plugin.py
@@ -27,10 +27,10 @@ class FeedPlugin(BasePlugin):
                 function=self.poll_rss_feed,
             )
 
-    async def send_notification(self, message):
-        """Sends a notification"""
-        if self.enabled:
-            await self.send_notification(message)
+    # async def send_notification(self, message):
+    #     """Sends a notification"""
+    #     if self.enabled:
+    #         await self.send_notification(message)
 
     def poll_rss_feed(self):
         """
@@ -43,4 +43,4 @@ class FeedPlugin(BasePlugin):
         for entry in feed.entries:
             updates = f"{entry.title} - {entry.link}"
             logger.debug("Updates: {}", updates)
-            return await self.send_notification(updates)
+            await self.send_notification(updates)
